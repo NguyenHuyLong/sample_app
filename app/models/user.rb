9852c26f6b279067cf988ef1e878af4ae093db5f
NguyenHuyLong <nguyen.huy.long@framgia.com>
2016-11-30 08:36:44 +0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save :downcase
 
   validates :name, presence: true, length: {maximum: 50}
@@ -11,16 +12,34 @@ class User < ActiveRecord::Base
 
   enum gender: [:female, :male]
 
-  private
-  def downcase
-    self.email.downcase
-  end
-
   class << self
     def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  private
+  def downcase
+    self.email.downcase
   end
 end
